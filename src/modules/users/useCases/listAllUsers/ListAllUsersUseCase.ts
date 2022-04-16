e128b21813679d517245b006bbedaa7f11f5364c
unknown <heenriquecds@gmail.com>
2022-04-16 15:30:58 -0300

    Giant commit with everything done

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,19 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+  execute({ user_id }: IRequest): User[] | undefined {
+    const user = this.usersRepository.findById(user_id)
+
+    if (!user) {
+      throw new Error("You need to be logged to retrieve users list!")
+    }
+    if (user.admin === false) {
+      throw new Error("Insufficient permissions")
+    }
+    const users = this.usersRepository.list()
+    return users;
   }
 }
 
